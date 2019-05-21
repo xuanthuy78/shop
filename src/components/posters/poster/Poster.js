006aa1eac2c96298c82eee81c0a808c53a63cf63
xuanthuy78 <phamthixuanthuy78@gmail.com>
2019-05-21 16:21:13 +0700

    feat(posters):add pagination poster

--- src/components/posters/poster/Poster.js
+++ src/components/posters/poster/Poster.js
@@ -4,24 +4,24 @@ import { Link } from "react-router-dom";
 export class Poster extends Component {
   render() {
     return (
-      <div className="Poster col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
-        <div className="card mb-3">
-          <img
-            className="card-img-top"
-            src="http://dummyimage.com/300x200.png/5fa2dd/ffffff"
-            alt="Card image"
-            style={{ width: "100%" }}
-            aria-hidden
-          />
-          <div className="card-body">
-            <h4 className="card-title">Ship Your Idea</h4>
-            <p className="card-text">$15.00</p>
-            <Link to="/" className="nav-link btn btn-primary">
-              Add to cart
-            </Link>
+        <div className="Poster col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+          <div to="/" className="card mb-3">
+            <img
+              className="card-img-top"
+              src="http://dummyimage.com/300x200.png/5fa2dd/ffffff"
+              alt="Card image"
+              style={{ width: "100%" }}
+              aria-hidden
+            />
+            <div className="card-body">
+              <h4 className="card-title">Ship Your Idea</h4>
+              <p className="card-text">$15.00</p>
+              <Link to="/" className="nav-link btn btn-dark">
+                Add to cart
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
     );
   }
 }
