b6edddcd46c5761186557907992af677f4f1b6a6
ToanTT <toantt@greenglobal.vn>
2019-05-22 14:29:50 +0700

    Updates

---
+++ precache-manifest.59376ced6ebd404d22c36727050b14f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8482fbf0cc5abded6b8ea8df67c081d",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "364698088b5ca4803e5c",
+    "url": "/shop/static/css/main.57b33028.chunk.css"
+  },
+  {
+    "revision": "1694dcfbb3c76fd258de",
+    "url": "/shop/static/js/2.cacc97b3.chunk.js"
+  },
+  {
+    "revision": "364698088b5ca4803e5c",
+    "url": "/shop/static/js/main.59605aed.chunk.js"
+  },
+  {
+    "revision": "162d8e4e66ca27cdb508",
+    "url": "/shop/static/js/runtime~main.265007fa.js"
+  },
+  {
+    "revision": "8141fbd965945ac4b23b20ee360536c0",
+    "url": "/shop/static/media/poster.8141fbd9.jpg"
+  }
+]);
